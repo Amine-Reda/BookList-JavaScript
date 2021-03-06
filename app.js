b88e7e7708bab081b89b8d4769d80d60d414de71
amine <amine.reda037@gmail.com>
2021-03-06 16:22:55 +0100

    add part2

--- app.js
+++ app.js
@@ -41,6 +41,12 @@ class UI {
     `;
     list.appendChild(row);
   }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
 }
 
 // Store Class: Handles Storage
@@ -50,5 +56,25 @@ class UI {
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 //Even: Add a Book
+
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  //  Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  // Instatiate book
+  const book = new Book(title, author, isbn);
+
+  // Add Boook to UI
+
+  UI.addBookToList(book);
+  // clear Field
+
+  UI.clearFields();
+});
 
 //Event: Remove a Book
